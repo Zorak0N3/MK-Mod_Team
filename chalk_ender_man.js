fe48bc1d5b2bf245470d04c6ffb97593624b1dd9
amato17 <amato17@naver.com>
2013-08-18 15:28:40 +0900

    초크의 다섯번째 스크립트

---
+++ chalk_ender_man.js
@@ -0,0 +1,28 @@
+// EnderMan Script 
+// By Chalk 
+// Ver 1.4
+// For MCPE Korea
+
+var pl = null;
+
+function useItem(x, y, z, itemId, blockId)
+{
+    if(itemId==262)
+    {
+		while(true)
+		{
+			var rx = Math.floor(Math.random() * 257);
+			var ry = Math.floor(Math.random() * 129);
+			var rz = Math.floor(Math.random() * 257);
+		
+			if(getTile(rx, ry , rz)==0&&getTile(rx, ry + 1, rz)==0&&getTile(rx, ry - 1, rz)!=0)
+			{
+				pl = getPlayerEnt();
+				setPosition(pl, rx, ry, rz);
+				addItemInventory(262, -1);
+				break;
+			}
+		}
+		
+	}
+}
