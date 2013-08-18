108d1f730ba23f4806ef31d50f7426f1615a0330
Kim Si Young <ksy4362@naver.com>
2013-08-18 22:07:12 +0900

    취급주의님 에러 수정

--- CGJY_SAW.js
+++ CGJY_SAW.js
@@ -1,31 +1,42 @@
-ver Player =null;
-function print("게임을 시작하지")
-function getCarriedItem()
+var Player = null;
+var RandomX = 0;
+var RandomY = 0;
+var RandomZ = 0;
+var tick = 0;
+var savetime= 0;
+
+function newLevel()
 {
-
-var RandomX = (int) (math.random( ) * 257);
-var RandomY = (int) (math.random( ) * 129);
-var RandomZ = (int) (math.random( ) * 257);
-
-while(true)
-{
-
-if(itemid == 264)
-{
-print("아깝군....잘가라");
-
-break;
+	print("게임을 시작하지");
 }
-else
-{
-setTile(RandomX,RandomY,RandomZ,0);
-                 
-getCarriedItem()
 
-try 
+function modTick()
 {
-sleep(5);
-}  catch(Exception e  )  { }
-} 
+	if(getYaw() == 0) return;
+	
+	tick++;
+	if(tick - savetime < 20) return;
+	
+	savetime = tick;
+	
+	if(getCarriedItem() == 264)
+	{
+		print("아깝군....잘가라");
+	}
+	else
+	{
+		newrandom();
+		
+		if(getTile(RandomX, RandomY, RandomZ) != 0)
+		{
+			setTile(RandomX, RandomY, RandomZ, 0);
+		}
+	}
 }
+
+function newrandom()
+{
+	RandomX = parseInt(math.random( ) * 256);
+	RandomY = parseInt(math.random( ) * 128);
+	RandomZ = parseInt(math.random( ) * 256);
 }
