c118e157d9a776889386fa0b4a2e4710b9a59feb
Kim Si Young <ksy4362@naver.com>
2013-08-18 21:40:33 +0900

    취급주의님의 쏘우 스크립트 추가

---
+++ CGJY_SAW.js
@@ -0,0 +1,31 @@
+ver Player =null;
+function print("게임을 시작하지")
+function getCarriedItem()
+{
+
+var RandomX = (int) (math.random( ) * 257);
+var RandomY = (int) (math.random( ) * 129);
+var RandomZ = (int) (math.random( ) * 257);
+
+while(true)
+{
+
+if(itemid == 264)
+{
+print("아깝군....잘가라");
+
+break;
+}
+else
+{
+setTile(RandomX,RandomY,RandomZ,0);
+                 
+getCarriedItem()
+
+try 
+{
+sleep(5);
+}  catch(Exception e  )  { }
+} 
+}
+}
