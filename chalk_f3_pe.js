ab8cd6b2fa35427c14b444436b72de6e11bcb713
amato17 <amato17@naver.com>
2013-08-18 14:59:44 +0900

    Create chalk_f3_pe.js
    초크의 두번째 스크립트

---
+++ chalk_f3_pe.js
@@ -0,0 +1,19 @@
+/* 제작자 : 초록크리퍼(amato17) - MCPE Korea
+ * 버젼 : 1.4
+ */
+
+function useItem(x, y, z, itemId, blockId) 
+{
+    if(itemId==339) 
+    {
+        var px = getPlayerX();
+		var py = getPlayerY();
+		var pz = getPlayerZ();
+		print("[F3 PE] Player Info - ID : " + itemId + "\nX : " + px  + "\nY : " + py + "\nZ : " + pz);
+		print("[F3 PE] Block Info - ID : " + blockId + ", X : " + x  + ", Y : " + y + ", Z : " + z);
+    }
+	else if(itemId==340)
+    {
+		print("[F3 PE] Block ID : " + blockId);
+    }
+}
