f9559346098d011b44d32c7230a5c241761392ab
amato17 <amato17@naver.com>
2013-08-18 15:48:10 +0900

    초크의 아홉번째 스크립트
    일회용 다이아몬드 도구

---
+++ chalk_9_disposable_diamond_tool.js
@@ -0,0 +1,16 @@
+// 초크의 9번째 스크립트
+// 일회용 다이아몬드 도구
+// 스크립트 버젼 : 1.3.0
+// chalk-disposable-diamond-tool.js
+
+function useItem( x , y , z , iID , bID )
+{
+    if( iID == 0 )
+    {
+		 addItemInventory( 276 , -1 );
+		 addItemInventory( 277 , -1 );
+		 addItemInventory( 278 , -1 );
+		 addItemInventory( 279 , -1 );
+		 print( "일회용 다이아몬드 도구를 지급하였습니다 - 제작자 : 초크" );
+	}
+}
