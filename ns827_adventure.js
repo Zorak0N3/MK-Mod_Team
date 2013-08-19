5bb57d38f7d4cdf04114af184387a11e27934143
Kim Si Young <ksy4362@naver.com>
2013-08-19 21:25:59 +0900

    남생이님 폴더 추가

--- ns827_adventure.js
+++
@@ -1,8 +0,0 @@
-//SCRIPT BY ns827@Mod Team
-
-preventDefault();
-function useItem(x,y,z,itemId,blockId)
-{
-preventDefault();
-clientMessage("어드벤처 모드입니다!");
-}
---
+++ 남생이/ns827_adventure.js
@@ -0,0 +1,8 @@
+//SCRIPT BY ns827@Mod Team
+
+preventDefault();
+function useItem(x,y,z,itemId,blockId)
+{
+preventDefault();
+clientMessage("어드벤처 모드입니다!");
+}
