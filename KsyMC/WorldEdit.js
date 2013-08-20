3b8b6d52403153331763cddb9b066ed2c878e48d
Kim Si Young <ksy4362@naver.com>
2013-08-20 16:42:48 +0900

    월드에딧 스크립트 업데이트

--- KsyMC/WorldEdit.js
+++ KsyMC/WorldEdit.js
@@ -1,135 +1,122 @@
 var selection1 = [0, 0, 0];
 var selection2 = [0, 0, 0];
- 
-var first = false;
-var second = false;
- 
+
+var select_first = false;
+var select_second = false;
+
 var set = false;
 var replace = false;
- 
+
 var saveItemId = 0;
- 
-function useItem(x, y, z, item, block)
-{
-	if(item == 292)
+
+function procCmd(cmd){
+	arg = cmd.toLowerCase().split(" ");
+	
+	switch(arg)
 	{
+		case "/set":
+			if(!arg[1].length){
+				chatMessage("사용법 : //set [아이템 ID]");
+			}
+			
+			var block = new Number(arg[1]);
+			
+			if(!select_first || !select_second){
+				chatMessage("선택된 영역이 없습니다.");
+			}
+			
+			chatMessage(W_set(block) + "개의 블록이 수정되었습니다.");
+			break;
+		case "/replace":
+			if(!arg[1].length || !arg[2].length){
+				chatMessage("사용법 : //replace [교체할 ID] [교체될 ID]");
+			}
+			
+			var block1 = new Number(arg[1]);
+			var block2 = new Number(arg[2]);
+			
+			if(!select_first || !select_second){
+				chatMessage("선택된 영역이 없습니다.");
+			}
+			
+			chatMessage(W_replace(block1, block2) + "개의 블록이 수정되었습니다.");
+			break;
+	}
+}
+
+function useItem(x, y, z, item, block){
+	if(item == 292){
 		saveItemId = block;
-		print("아이템 ID : "+block);
+		chatMessage("아이템 ID : "+block);
 		preventDefault();
 	}
 	
-	if(item == 267)
-	{
-		if(!first)
-		{
-			first = true;
+	if(item == 267){
+		if(!select_first){
+			select_first = true;
 			selection1[0] = Math.round(x);
 			selection1[1] = Math.round(y);
 			selection1[2] = Math.round(z);
-			print("첫번째 선택");
-		}
-		else if(!second)
-		{
-			second = true;
+			
+			chatMessage("첫번째 선택 완료.\n(X : " + selection1[0] + " Y : " + selection1[1] + " Z : " + selection1[2] + ")");
+		}else if(!select_second){
+			select_second = true;
 			selection2[0] = Math.round(x);
 			selection2[1] = Math.round(y);
 			selection2[2] = Math.round(z);
-			print("두번째 선택");
-		}
-		else
-		{
-			first = false;
+			
+			chatMessage("두번째 선택 완료.\n(X : " + selection1[0] + " Y : " + selection1[1] + " Z : " + selection1[2] + ")");
+		}else{
+			select_first = false;
 			second = false;
-			print("초기화 완료");
-		}
-	}
-	
-	if(first && second)
-	{
-		if(item == 16) // set
-		{
-			if(set)
-			{
-				W_set(block);
-				
-				set = false;
-			}
-			else
-			{
-				clientMessage("Set : 선택한 지역을 "+block+"블럭으로 변경 하시겠습니까?");
-				set = true;
-			}
-			preventDefault();
-		}
-		
-		if(item == 15) // replace
-		{
-			if(replace)
-			{
-				W_replace(saveItemId, block);
-				
-				replace = false;
-			}
-			else
-			{
-				clientMessage("Replace : 선택한 지역에서 "+saveItemId+"블럭을 "+block+"블럭으로 변경 하시겠습니까?");
-				replace = true;
-			}
-			preventDefault();
+			
+			chatMessage("선택 지점이 초기화 되었습니다.\n다시 선택해 주세요.");
 		}
 	}
 }
- 
-function W_set(block)
-{
-	var startX = _mathMin(selection1[0], selection2[0]);
-	var endX = _mathMax(selection1[0], selection2[0]);
-	var startY = _mathMin(selection1[1], selection2[1]);
-	var endY = _mathMax(selection1[1], selection2[1]);
-	var startZ = _mathMin(selection1[2], selection2[2]);
-	var endZ = _mathMax(selection1[2], selection2[2]);
+
+function W_set(block){
+	var startX = Math.min(selection1[0], selection2[0]);
+	var endX = Math.max(selection1[0], selection2[0]);
+	var startY = Math.min(selection1[1], selection2[1]);
+	var endY = Math.max(selection1[1], selection2[1]);
+	var startZ = Math.min(selection1[2], selection2[2]);
+	var endZ = Math.max(selection1[2], selection2[2]);
 	
-	for(var x = startX; x <= endX; x++)
-	{
-		for(var y = startY; y <= endY; y++)
-		{
-			for(var z = startZ; z <= endZ; z++)
-			{
+	var count = 0;
+	
+	for(var x = startX; x <= endX; x++){
+		for(var y = startY; y <= endY; y++){
+			for(var z = startZ; z <= endZ; z++){
 				setTile(x, y, z, block);
+				count++;
 			}
 		}
 	}
-}
- 
-function W_replace(block1, block2)
-{
-	var startX = _mathMin(selection1[0], selection2[0]);
-	var endX = _mathMax(selection1[0], selection2[0]);
-	var startY = _mathMin(selection1[1], selection2[1]);
-	var endY = _mathMax(selection1[1], selection2[1]);
-	var startZ = _mathMin(selection1[2], selection2[2]);
-	var endZ = _mathMax(selection1[2], selection2[2]);
 	
-	for(var x = startX; x <= endX; x++)
-	{
-		for(var y = startY; y <= endY; y++)
-		{
-			for(var z = startZ; z <= endZ; z++)
-			{
-				var targetId = getTile(x, y, z);
-				if(targetId == block1)
-				{
+	return count;
+}
+
+function W_replace(block1, block2){
+	var startX = Math.min(selection1[0], selection2[0]);
+	var endX = Math.max(selection1[0], selection2[0]);
+	var startY = Math.min(selection1[1], selection2[1]);
+	var endY = Math.max(selection1[1], selection2[1]);
+	var startZ = Math.min(selection1[2], selection2[2]);
+	var endZ = Math.max(selection1[2], selection2[2]);
+	
+	for(var x = startX; x <= endX; x++){
+		for(var y = startY; y <= endY; y++){
+			for(var z = startZ; z <= endZ; z++){
+				var block = getTile(x, y, z);
+				
+				if(block == block1){
 					setTile(x, y, z, block2);
+					count++;
 				}
 			}
 		}
 	}
-}
- 
-function _mathMin(first, second) {
-	return first < second? first : second;
-}
- 
-function _mathMax(first, second) {
-	return first > second? first : second;
+	
+	return count;
 }
