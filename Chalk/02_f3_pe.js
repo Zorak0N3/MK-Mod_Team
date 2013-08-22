ec242be2b3cae9f6f0b9160a792520d973e98efb
amato17 <amato17@naver.com>
2013-08-22 20:31:24 +0900

    이벤트 무시

--- Chalk/02_f3_pe.js
+++ Chalk/02_f3_pe.js
@@ -1,19 +1,21 @@
 /* 제작자 : 초록크리퍼(amato17) - MCPE Korea - 2
- * 버젼 : 1.3.0
+ * 버젼 : 1.3.1
  */
 
 function useItem(x, y, z, itemId, blockId) 
 {
     if(itemId==339) 
     {
+    	preventDefault();
         var px = getPlayerX();
-		var py = getPlayerY();
-		var pz = getPlayerZ();
-		print("[F3 PE] Player Info - ID : " + itemId + "\nX : " + px  + "\nY : " + py + "\nZ : " + pz);
-		print("[F3 PE] Block Info - ID : " + blockId + ", X : " + x  + ", Y : " + y + ", Z : " + z);
+	var py = getPlayerY();
+	var pz = getPlayerZ();
+	print("[F3 PE] Player Info - ID : " + itemId + "\nX : " + px  + "\nY : " + py + "\nZ : " + pz);
+	print("[F3 PE] Block Info - ID : " + blockId + ", X : " + x  + ", Y : " + y + ", Z : " + z);
     }
-	else if(itemId==340)
+    else if(itemId==340)
     {
-		print("[F3 PE] Block ID : " + blockId);
+    	preventDefault();
+	print("[F3 PE] Block ID : " + blockId);
     }
 }
