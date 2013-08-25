43c4efd62c47a92af3657b4f2315ef0fbb8fdc86
Park Jun Young <amato17@naver.com>
2013-08-25 12:12:05 +0900

    테스트중

---
+++ Chalk/13_high_dot_TEST.js
@@ -0,0 +1,47 @@
+//ver 0.1
+
+var byVal = 30;
+var upItem = 267;
+var downItem = 292;
+
+function newLevel()
+{
+	print("[highdot] 제작자 : 초크(amato17)\n철칼로 상승, 철괭이로 하강\n이동할 거리 설정 - /highdot 이동할 거리");
+}
+
+function useItem( x , y , z , i , b )
+{
+	if( i == upItem )
+	{
+		setVelY( getPlayerEnt() , byVal );
+	}
+	else if( i == downItem )
+	{
+		setVelY( getPlayerEnt() , -byVal );
+	}
+}
+
+function procCmd( cmd )
+{
+	cutcmd = cmd.split( ' ' );
+	
+	if( cutcmd[0] == "highdot" )
+	{
+		if( !(isNaN( cutcmd[1] ) ) )
+		{
+			byVal = cutcmd[1];
+			print("이동 거리 : " + byVal + "칸");
+		}
+		else
+		{
+			if( cutcmd[1] == "help" )
+			{
+				print("철칼로 상승, 철괭이로 하강\n이동할 거리 설정 - /highdot 이동할 거리");
+			}
+			else
+			{
+				print("이동할 거리는 숫자로 설정해야 합니다");
+			}
+		}
+	}
+}
