ae15e75d1623d49fa5aa181a7936a2c4efe05e74
Park Jun Young <amato17@naver.com>
2013-08-25 12:24:31 +0900

    Bouncy Bed 완성

--- Chalk/12_bouncy_bed.js
+++ Chalk/12_bouncy_bed.js
@@ -1,8 +1,14 @@
 // Bouncy Bed
 // By Chalk
-// Current Version : 0.8
+// Current Version : 0.9
 
 var val = 0.1;
+
+function newLevel()
+{
+	print("\n[BouncyBed]\n제작자 : 초크\n침대의 가운데를 밟지 않으면 위험할 수 있습니다");	
+	
+}
 
 function modTick()
 {
