79ae48867134269d5c931b5b6e0b3afe86cfaf07
Park Jun Young <amato17@naver.com>
2013-08-25 12:04:45 +0900

    update 0.7

--- Chalk/12_bouncy_bed_TEST.js
+++ Chalk/12_bouncy_bed_TEST.js
@@ -1,22 +1,15 @@
 // Bouncy Bed
 // By Chalk
-// Current Version : 0.6
+// Current Version : 0.7
 
-var val = 1;
-var del = 0;
+var val = 0.1;
+
 function modTick()
 {
-	if( getTile( Player.getX() , Player.getY() - 2 , Player.getZ() ) == 26 && del = 0 )
+	if( getTile( Player.getX() , Player.getY() - 2 , Player.getZ() ) == 26 )
 	{
 			Entity.setVelY( Player.getEntity() , val );
-			val += 0.1;
-			del = 1;
-	}
-	else if( getTile( Player.getX() , Player.getY() - 2 , Player.getZ() ) == 0 && del = 1 )
-	{
-		del = 0;
-		
-	}
+			val += 0.01;
 
-
+	}
 }
