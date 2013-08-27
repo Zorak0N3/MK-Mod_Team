8f3565893f7b0eb8b56d2c36a95afca31693831c
Park Jun Young <amato17@naver.com>
2013-08-28 07:56:05 +0900

    1.4 업데이트 - 문구 수정

--- Chalk/08_trash_can.js
+++ Chalk/08_trash_can.js
@@ -1,7 +1,12 @@
 // 초크의 8번쨰 스크립트
 // Obsidian TrashCan
 // MCPE Korea Only
-// Current Version : 1.3.2
+// Current Version : 1.4
+
+function newLevel()
+{
+	print("\n[쓰레기통]\n옵시디언에 아이템을 버려라!\nBy Chalk(amato17)");
+}
 
 function useItem( x , y , z , iID , bID )
 {
