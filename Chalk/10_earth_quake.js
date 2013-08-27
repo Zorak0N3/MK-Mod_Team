c51568bddc73dfccc9acfa63fc7de6f7cd84cafb
Park Jun Young <amato17@naver.com>
2013-08-28 07:50:13 +0900

    1.4 업데이트 - 문구 수정

--- Chalk/10_earth_quake.js
+++ Chalk/10_earth_quake.js
@@ -1,15 +1,19 @@
 // 초크의 10번째 공식 스크립트
 // 제목 : 지진 ( earthquake )
-// 버젼 : 1.3.0
+// 버젼 : 1.4
 // MCPE Korea
 
-var pl;
+function newLevel()
+{
+	print("\n[지진]\n제작자 : 초크\n금부츠를 들면 지진이 일어납니다\n나는 중에는 사용하지 마세요");
+	
+}
 
 function modTick()
 {
     if( getCarriedItem() == 317 ) // Gold Boots
     {
-		pl = getPlayerEnt();
-		setPosition( pl , getPlayerX() , getPlayerY() + 0.1 , getPlayerZ() );	
+		
+		setPosition( getPlayerEnt() , getPlayerX() , getPlayerY() + 0.1 , getPlayerZ() );	
 	}
 }
