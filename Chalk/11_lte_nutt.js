5ff147012c6b75750e286e91f5d501959ee82dfe
Park Jun Young <amato17@naver.com>
2013-08-28 07:47:47 +0900

    1.4 업데이트 - 문구 수정 및 오류 수정 ( 딜레이 )

--- Chalk/11_lte_nutt.js
+++ Chalk/11_lte_nutt.js
@@ -1,18 +1,16 @@
 // LTE NUTT Script - 11th Script
 // Made By Chalk(amato17) - MCPE Korea Mod Team
 // Idea By SK Telecom - LTE NUTT APP
-// Current Version = 1.3.0
+// Current Version = 1.4
 
 var startYaw;
 var curYaw;
 var isShakeStart = 0;
-var delay = 45;
+var delay = 150;
 
 function newLevel()
 {
-	// startYaw = getYaw();
-	print("[LTE 눝]\nIdea By SKT\nMade By Chalk(amato17)");
-
+	print("\n[LTE 눝]\nIdea By SKT\nBy Chalk(amato17)");
 }
 
 function modTick()
@@ -23,27 +21,27 @@ function modTick()
 		{
 			isShakeStart = 1;
 			startYaw = getYaw();
-			print("[LTE 눝] 눝돌리기 시작!");
+			print("\n[LTE 눝]\n눝돌리기 시작!");
 		}
 	}
 	else if(getCarriedItem() == 6 && isShakeStart == 1 )
 	{
 		curYaw = getYaw();
+		
 		if( getMax( curYaw , startYaw ) - getMin( curYaw , startYaw ) >= 1080 )
 		{
 			addItemInventory( 6 , -1 );
 			addItemInventory( 17 , 5 );
 			isShakeStart = 0;
-			print("[LTE 눝] 눝눝누누눝!");
-			delay = 45;
+			print("\n[LTE 눝]\n나무를 만들었습니다!\n눝눝누누눝!");
+			delay = 150;
 		}
 	
 	}
 	
-	if( delay > 0 )
+	if( delay > 0 && isShakeStart  == 0 )
 	{
 		delay--;
-		
 	}
 
 }
