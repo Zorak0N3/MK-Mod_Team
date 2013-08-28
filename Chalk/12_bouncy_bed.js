53e47c8c6d15e5a438761f40ad0ee33881a493d3
Park Jun Young <amato17@naver.com>
2013-08-28 16:03:53 +0900

    1.2 업데이트 - 변수 수정

--- Chalk/12_bouncy_bed.js
+++ Chalk/12_bouncy_bed.js
@@ -1,24 +1,24 @@
 // Bouncy Bed
 // By Chalk
-// Current Version : 1.1
+// Current Version : 1.2
 
-var val = 0.1;
+var up = 0.1;
 
 function newLevel()
 {
-	print("\n[BouncyBed]\n제작자 : 초크\n침대의 가운데를 밟지 않으면 낙사데미지를 입습니다");	
+	print( "\n[BouncyBed]\n제작자 : 초크\n침대의 가운데를 밟지 않으면 낙사데미지를 입습니다" );	
 }
 
-function useItem( x, y, z, i, b )
+function useItem( x , y , z , itemID , blockID )
 {
-	val = 0.1;
+	up = 0.1;
 }
 
 function modTick()
 {
 	if( getTile( getPlayerX() , getPlayerY() - 2 , getPlayerZ() ) == 26 )
 	{
-			setVelY( getPlayerEnt() , val );
-			val += 0.1;
+			setVelY( getPlayerEnt() , up );
+			up += 0.1;
 	}
 }
