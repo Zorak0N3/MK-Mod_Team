181ced2dce3aebbc66e62a1076c23c23c3cf133d
Park Jun Young <amato17@naver.com>
2013-08-29 19:59:57 +0900

    0.1 업데이트 - 릴리즈

---
+++ Chalk/Test/A1_bar_bar_bar.js
@@ -0,0 +1,85 @@
+// Crayon Pop - Bar Bar Bar
+// Pop Pop Crayon Pop - Get Set Ready Go!
+// 0.1
+
+var px;
+var py;
+var pz;
+var a;
+var b;
+var c;
+var d;
+var e;
+var time = 30;
+
+function procCmd( cmd )
+{
+	if( cmd == "팝 팝 크레용팝" )
+	{
+		px = getPlayerX();
+		py = getPlayerY() + 5;
+		pz = getPlayerZ();
+		
+		print("\nGet,");
+		
+		a = spawnCow( px - 2 , py , pz , "mob/cow.png" );
+		b = spawnCow( px - 1 , py , pz , "mob/cow.png" );
+		c = spawnCow( px + 0 , py , pz , "mob/cow.png" );
+		d = spawnCow( px + 1 , py , pz , "mob/cow.png" );
+		e = spawnCow( px + 2 , py , pz , "mob/cow.png" );
+		
+		print("\nSet,");
+		
+		rideAnimal( a , a );
+		rideAnimal( b , b );
+		rideAnimal( c , c );
+		rideAnimal( d , d );
+		rideAnimal( e , e );
+		
+		print("\nReady,");
+		
+		go = true;
+		
+		print("\nGo!");
+	
+	
+	}
+
+}
+
+function modTick()
+{
+	setVelY( a , 0 );
+	setVelY( b , 0 );
+	setVelY( c , 0 );
+	setVelY( d , 0 );
+	setVelY( e , 0 );
+	
+	if( go )
+	{
+		if( time > 0 )
+		{
+			time--;
+			
+			if( time == 15 )
+			{
+				setVelY( a , 2 );
+				setVelY( b , -2 );
+				setVelY( c , 2 );
+				setVelY( d , -2 );
+				setVelY( e , 2 );
+			}
+			
+		}
+		else
+		{
+			setVelY( a , -2 );
+			setVelY( b , 2 );
+			setVelY( c , -2 );
+			setVelY( d , 2 );
+			setVelY( e , -2 );
+			time = 30;
+		}
+	
+	}
+}
