2d15c48a2475660fe87456e9730277f4622623df
Park Jun Young <amato17@naver.com>
2013-08-29 23:53:44 +0900

    Update A1_bar_bar_bar.js

--- Chalk/Test/A1_bar_bar_bar.js
+++ Chalk/Test/A1_bar_bar_bar.js
@@ -21,33 +21,71 @@ function procCmd( cmd )
 	if( cmd == "팝 팝 크레용팝" )
 	{
 		px = getPlayerX();
-		py = getPlayerY() + 5;
+		py = getPlayerY();
 		pz = getPlayerZ();
 		
-		print("\nGet,");
+		print("\nGet - 소 5마리가 스폰됩니다");
 		
-		a = spawnCow( px - 2 , py , pz , "mob/cow.png" );
-		b = spawnCow( px - 1 , py , pz , "mob/cow.png" );
-		c = spawnCow( px + 0 , py , pz , "mob/cow.png" );
-		d = spawnCow( px + 1 , py , pz , "mob/cow.png" );
-		e = spawnCow( px + 2 , py , pz , "mob/cow.png" );
+		spawnCow( px - 4 , py , pz , "mob/cow.png" );
+		spawnCow( px - 2 , py , pz , "mob/cow.png" );
+		spawnCow( px + 0 , py , pz , "mob/cow.png" );
+		spawnCow( px + 2 , py , pz , "mob/cow.png" );
+		spawnCow( px + 4 , py , pz , "mob/cow.png" );
 		
-		print("\nSet,");
+		print("\nSet - 스폰된 소 5마리를 때려주세요");
 		
-		rideAnimal( a , a );
-		rideAnimal( b , b );
-		rideAnimal( c , c );
-		rideAnimal( d , d );
-		rideAnimal( e , e );
-		
-		print("\nReady,");
-		
-		go = true;
-		
-		print("\nGo!");
+		attReady = 1;
 	
 	}
 
+}
+
+function attackHook( attacker , victim )
+{
+	switch( attReady )
+	{
+		case 1 :
+			a = victim;
+			rideAnimal( a , a );
+			attReady = 2;
+			print("\n첫번째 소를 설정했습니다");
+			break;
+		case 2 :
+			b = victim;
+			rideAnimal( b , b );
+			attReady = 3;
+			print("\n두번째 소를 설정했습니다");
+			break;
+		case 3 :
+			c = victim;
+			rideAnimal( c , c );
+			attReady = 4;
+			print("\n세번째 소를 설정했습니다");
+			break;
+		case 4 :
+			d = victim;
+			rideAnimal( d , d );
+			print("\n네번째 소를 설정했습니다");
+			attReady = 5;
+			break;
+		case 5 :
+			e = victim;
+			rideAnimal( e , e );
+			print("\n다섯번째 소를 설정했습니다");
+			attReady = 6;
+			
+			print("\nReady");
+		
+			go = true;
+			
+			
+		
+			print("\nGo!");
+			
+			break;
+			
+	}
+	
 }
 
 function modTick()
