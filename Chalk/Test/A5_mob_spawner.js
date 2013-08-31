3536cb97412cdfe4d8d0a5571737bf8342f4ee75
Park Jun Young <amato17@naver.com>
2013-08-31 22:13:46 +0900

    0.2 업데이트 - 에러 수정

--- Chalk/Test/A5_mob_spawner.js
+++ Chalk/Test/A5_mob_spawner.js
@@ -11,9 +11,10 @@ function useItem( x , y , z , i , b )
 		sy=y; 
 		sz=z; 
 		
-		on=1; 
+		on = on ? false : true; 
+		var msg = on ? " " : " 비";
 		
-		print("소 스폰 몹스포너가 생성되었습니다");
+		print("소 스폰 몹스포너가"+ msg +"활성화되었습니다");
 	}
 }
 
@@ -21,40 +22,41 @@ function modTick()
 {
 	if(on)
 	{
-		if(tc == 0 )
-		{
-			setTile( sx , sy , sz , 49 );
-			tc = Math.floor(Math.random() * 600);
-			mobSpawner();
-		}
-		else
+		if(tc > 0 )
 		{
 			setTile( sx , sy , sz , 246 );
 			tc--;
+		
+		}
+		else
+		{
+			setTile( sx , sy , sz , 49 );
+			tc = 150;
+			mobSpawner();
 		}
 	}
 }
-
+/*
 function attackHook( attacker , victim )
 {
 	tm = 0;
 }
-
+*/
 function mobSpawner()
 {
-	if( Math.abs( getPlayerX() - sx ) <= 17 && Math.abs( getPlayerY() - sy ) <= 17 && Math.abs( getPlayerZ() - sz ) <= 17 )
-	{
+	//if( Math.abs( getPlayerX() - sx ) <= 17 && Math.abs( getPlayerY() - sy ) <= 17 && Math.abs( getPlayerZ() - sz ) <= 17 )
+	//{
 		var mx = sx + ( Math.random() - Math.random() ) * 4;
 		var my = sy; // -1 0 +1 중 하나이지만 패스
 		var mz = sz + ( Math.random() - Math.random() ) * 4;
 		
-		if( getTile( mx , my - 1 , mz ) == 2 )
-		{
-			if(tm < 7 )
-			{
+		//if( getTile( mx , my - 1 , mz ) == 2 )
+		//{
+			//if(tm < 7 )
+			//{
 				spawnCow( mx , my , mz , 'mob/cow.png' );
-				tm++;
-			}
-		}
-	}
+				//tm++;
+			//}
+		//}
+	//}
 }
