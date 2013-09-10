57691ea08f698e03ca80d3cc16512ad2b0113374
Kim Si Young <ksy4362@naver.com>
2013-09-10 21:13:05 +0900

    히로빈 스크립트 업데이트

--- KsyMC/Scripts/Herobrine.js
+++ KsyMC/Scripts/Herobrine.js
@@ -1,20 +1,28 @@
 var block_HB = [];
 var HB = null;
+var spawnCount = 0;
+
 var TimerEnd = false;
 var Timer_time = 0;
 var Timer_tag = "";
 
 function useItem(x, y, z, item, block, side){
 	if(checkHB_Block(x, y - 3, z)){
+		preventDefault();
+		
 		if(block_HB.length != 0 && block_HB[0] == x && block_HB[1] == y - 3 && block_HB[2] == z) return;
 		
 		clientMessage("You don't know what you did....");
 		setHB_block(x, y - 3, z);
-		preventDefault();
 	}
 }
 
+function attackHook(attacker, victim){
+	if(spawnCount > 4 && victim.getYaw(victim) == HB.getYaw(HB)) setVelY(attacker, 1);
+}
+
 function modTick(){
+	// 타이머
 	if(Timer_time != -1){
 		Timer_time--;
 		
@@ -28,24 +36,39 @@ function modTick(){
 	
 	if(block_HB.length == 0) return;
 	
-	if(getTile(block_HB[0], block_HB[1], block_HB[2]) != 41 || getTile(block_HB[0], block_HB[1] + 1, block_HB[2]) != 41 || getTile(block_HB[0], block_HB[1] + 2, block_HB[2]) != 87 || getTile(block_HB[0], block_HB[1] + 3, block_HB[2]) != 87){
+	// 삭제
+	if(getTile(block_HB[0], block_HB[1], block_HB[2]) != 41 || getTile(block_HB[0], block_HB[1] + 1, block_HB[2]) != 41 || getTile(block_HB[0], block_HB[1] + 2, block_HB[2]) != 87 || getTile(block_HB[0], block_HB[1] + 3, block_HB[2]) != 87 || getTile(block_HB[0], block_HB[1] + 4, block_HB[2]) != 51){
 		block_HB = [];
 	}
 	
-	if(getTile(block_HB[0], block_HB[1] + 4, block_HB[2]) != 51){
-		setTile(block_HB[0], block_HB[1] + 4, block_HB[2], 51);
-	}
-	
-	if(parseInt(Math.random() * 1000) == 50 && HB == null){
-		HB = bl_spawnMob(getPlayerX() - 2, getPlayerY(), getPlayerZ(), 32, "mob/char.png");
-		clientMessage("HI!");
+	// 히로빈 소환
+	if(getRandom(0, 500) == 100 && HB == null){
+		spawnCount++;
 		
-		startTimer(5*20, "Herobrine remove");
+		switch(spawnCount){
+			case 1:
+				clientMessage("HI!");
+				break;
+			case 2:
+				clientMessage("!nileppeZ deL ot netsiL");
+				break;
+			case 6:
+				clientMessage("?mA I erehW");
+				HB_attackFire();
+				break;
+			default:
+				clientMessage("!lleh ot emoclew");
+				break;
+		}
+		
+		HB = bl_spawnMob(getPlayerX() - 2, getPlayerY(), getPlayerZ(), 32, "mob/char.png");
+		startTimer(getRandom(3, 7), "HB_remove");
 	}
 	
+	// 타이머 종료 처리
 	if(TimerEnd){
 		switch(getTimerTag()){
-			case "Herobrine remove":
+			case "HB_remove":
 				removeHB();
 				break;
 		}
@@ -53,12 +76,13 @@ function modTick(){
 }
 
 function newLevel(){
-	
+	print("\nHerobrine 1.0 Beta By 제조일자");
 }
 
 function leaveGame(){
 	block_HB = [];
 	HB = null;
+	spawnCount = 0;
 }
 
 function removeHB(){
@@ -76,18 +100,28 @@ function checkHB_Block(floorX, floorY, floorZ){
 function setHB_block(floorX, floorY, floorZ){
 	block_HB = [floorX, floorY, floorZ];
 	setTile(floorX, floorY + 4, floorZ, 51);
+	explode(floorX, floorY + 5, floorZ, 0.01);
+}
+
+function HB_attackFire(){
+	setTile(getPlayerX() + 2, getPlayerY() - 1 , getPlayerZ(), 51);
+	setTile(getPlayerX(), getPlayerY() - 1, getPlayerZ() + 5, 51);
+	setTile(getPlayerX(), getPlayerY() - 1, getPlayerZ(), 51);
+	setTile(getPlayerX() + 5, getPlayerY() - 1, getPlayerZ() + 1, 51);
+	setTile(getPlayerX() + 1, getPlayerY() - 1, getPlayerZ(), 51);
+	explode(getPlayerX(), getPlayerY(), getPlayerZ(), 0.01);
 }
 
 /*
-  타이머 기능 함수
+  타이머 함수
   
   startTimer(시간, 식별 태그)
   getTimerTime() 현재 작동중인 타이머의 시간을 불러옵니다.
   getTimerTag 현재 작동중인 타이머의 태그를 불러옵니다
 */
 
-function startTimer(time, tag){
-	Timer_time = time;
+function startTimer(sec, tag){
+	Timer_time = sec * 20;
 	Timer_tag = tag;
 }
 
@@ -99,7 +133,11 @@ function getTimerTag(){
 	return Timer_tag;
 }
 
-/* 타이머 기능 함수 END */
+/* 타이머 함수 END */
+
+function getRandom(start, end){
+	return parseInt(Math.random() * (end - start)) + start;
+}
 
 function getSide(x, y, z, side){
 	var pos = new Array(2);
