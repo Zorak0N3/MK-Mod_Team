188197adc43ab3c521d8a2b6e855939baf94ca73
smk020 <smk020@naver.com>
2013-09-21 09:37:10 +0900

    데이즈 업뎃 그러나 아직 제작중

--- smk020/Scripts/DAYZ.js
+++ smk020/Scripts/DAYZ.js
@@ -2,13 +2,21 @@
 곱둥연가에게만 있습니다 */
 var a = 0;
 var chestT = 0;
-var T = 0;
 var playerE = null;
 var zombieE = null;
+var zombieE2 = null;
+var zombieE3 = null;
+var zombieE4 = null;
+var zombieE5 = null;
+var zombieE6 = null;
+var zombieE7 = null;
+var zombieE8 = null;
 var bullet = 30;
-var playerH = 20;
-var Radioactivity = 0;
+var D = 0;
+var hunger = 20;
+var htimer = 900;
 var playerL = new Array(3);
+var playerH = 20;
 var zombieL = new Array(3);
 
 var L = new Array(3);
@@ -18,8 +26,7 @@ var chest = true;
 function newLevel()
 {
 chestTcheck();
-ModPE.setItem(392,0,15,"사냥총");
-ModPE.setItem(393,1,15,"방사능측정기");
+setitem();
 }
 function leaveGame()
 {
@@ -28,31 +35,17 @@ chest = true;
 }
 function getlocation()
 {
-playerL[0] = getPlayerX();
-playerL[1] = getPlayerY();
-playerL[2] = getPlayerZ();
+playerL = [getPlayerX(),getPlayerY(),getPlayerZ()]
 }
 function modTick()
 {
-setPlayerHealth(playerH);
-if(Radioactivity>500)
+if(playerH==0)
 {
-playerH = playerH - 4;
-
-Radioactivity = 0;
+playerH = 20;
 }
+checkRadioactivity();
 chestTcheck();
 getlocation();
-if(getCarriedItem()==393)
-{
-T = T + 1;
-}
-if(T==150)
-{
-Radioactivity = parseInt(Math.random() * 1000);
-clientMessage("방사능농도:" + Radioactivity);
-T = 0;
-}
 if(getTile(120,5,89) == 247)
 {
 chest = false;
@@ -73,10 +66,15 @@ a = 1;
 }
 function useItem(x,y,z,i,b,s)
 {
+if(i == 394)
+{
+setTile(x,y,z,253);
+}
 if(b==54 && chestT==0)
 {
 addItemInventory(393,1);
 addItemInventory(392,1);
+addItemInventory(394,1);
 setTile(1,1,50,246);
 }
 
@@ -123,7 +121,19 @@ sin=-Math.sin(yaw/180*Math.PI);
 cos=Math.cos(yaw/180*Math.PI);
 tan=-Math.sin(pitch/180*Math.PI);
 pcos=Math.cos(pitch/180*Math.PI);
+
+explode(px+4*sin*pcos,py+4*tan,pz+4*cos*pcos,1);
 explode(px+5*sin*pcos,py+5*tan,pz+5*cos*pcos,2);
+explode(px+6*sin*pcos,py+6*tan,pz+6*cos*pcos,2);
+explode(px+7*sin*pcos,py+7*tan,pz+7*cos*pcos,2);
+explode(px+8*sin*pcos,py+8*tan,pz+8*cos*pcos,2);
+explode(px+9*sin*pcos,py+9*tan,pz+9*cos*pcos,2);
+explode(px+10*sin*pcos,py+10*tan,pz+10*cos*pcos,2);
+explode(px+11*sin*pcos,py+11*tan,pz+11*cos*pcos,2);
+explode(px+12*sin*pcos,py+12*tan,pz+12*cos*pcos,2);
+explode(px+14*sin*pcos,py+14*tan,pz+14*cos*pcos,2);
+explode(px+16*sin*pcos,py+16*tan,pz+16*cos*pcos,2);
+explode(px+18*sin*pcos,py+18*tan,pz+18*cos*pcos,2);
 bullet = bullet - 1;
 }
 }
@@ -131,4 +141,18 @@ function setitem()
 {
 ModPE.setItem(392,0,15,"사냥총");
 ModPE.setItem(393,1,15,"방사능측정기");
+ModPE.setItem(394,2,15,"방사능폐기물");
+}
+function checkRadioactivity()
+{
+if(getTile(playerL[0],playerL[1]-2,playerL[2])==253)
+{
+D = D + 1;
+}
+if(D==5)
+{
+D = 0;
+playerH = playerH - 2;
+setPlayerHealth(playerH);
+}
 }
